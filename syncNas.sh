ec296bba6326be721afc70c6ffff401725658e23
anden <anden@prometheus.local>
2016-11-09 00:25:38 +0100

    some cleanup plus fix to ignore Podcasts folder adds xld

--- syncNas.sh
+++ syncNas.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-rsync -avhrWAHxv --exclude '.DS_Store' --exclude '/Volumes/Storage/Music/Podcasts/*' --numeric-ids --progress --inplace --delete --log-file=syncNas.log /Volumes/Storage/Music admin@nas.local:/share/Multimedia
+rsync -avhrWAHxv \
+--exclude '.DS_Store' \
+--exclude 'Podcasts' \
+--numeric-ids --progress --inplace --delete \
+--log-file=syncNas.log \
+/Volumes/Storage/Music admin@nas.local:/share/Multimedia
 
