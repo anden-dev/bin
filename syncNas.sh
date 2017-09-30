3f95741f4ab764bf5edff934b95b8fe02971ba4f
anden <anden.dev@gmail.com>
2017-09-30 19:09:18 +0200

    adds --delete and ignores Vogelstimmen dir

--- syncNas.sh
+++ syncNas.sh
@@ -3,7 +3,9 @@
 rsync -avhrWAHxv \
 --exclude '.DS_Store' \
 --exclude 'Podcasts' \
+--exclude 'Vogelstimmen' \
 --numeric-ids --progress --inplace --delete \
 --log-file=syncNas.log \
+--delete
 /Volumes/Storage/Music admin@nas.local:/share/Multimedia
 
