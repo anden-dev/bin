25e793b73238a06eee8e4b293cd47dd968aeac6e
anden <anden.dev@gmail.com>
2017-10-04 23:05:31 +0200

    updates exclude patterns

--- syncNas.sh
+++ syncNas.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+>$DIR/last_syncNas.log
 
 rsync -avhrWAHxv \
 --exclude '.DS_Store' \
---exclude 'Podcasts' \
---exclude 'Vogelstimmen' \
+--exclude 'Podcasts/' \
+--exclude '*Vogelstimmen*' \
+--exclude '*(SACD)*' \
+--exclude '*(DCC)*' \
+--exclude '*(XRCD)*' \
+--exclude '*(XRCD24)*' \
+--exclude '*(MFSL)*' \
+--exclude '*Wagner*' \
+--exclude '*Mozart*' \
 --numeric-ids --progress --inplace --delete \
---log-file=syncNas.log \
---delete
-/Volumes/Storage/Music admin@nas.local:/share/Multimedia
-
+--log-file=$DIR/last_syncNas.log \
+/Volumes/Storage/Music admin@nas.local:/share/Multimedia \
+--delete-before --delete-excluded 
+#--exclude 'Compilations/' \
